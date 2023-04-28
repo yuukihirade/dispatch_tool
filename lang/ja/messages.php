85faf02fca7f4834fca481b168c609a047f18f0e
y-hirade <yuuki.h.0909@gmail.com>
2023-04-28 13:01:41 +0000

    #6 ユーザー登録画面を変更

---
+++ lang/ja/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'login' => 'ログイン',
+  'logout' => 'ログアウト',
+  'name' => '名前',
+  'email' => 'メールアドレス',
+  'password' => 'パスワード',
+  'remember_me' => 'ログイン情報を記憶する',
+  'forgot_your_password?' => 'パスワードを忘れてしまった場合',
+  'confirm_password' => 'パスワードの確認',
+  'register' => 'ユーザー登録',
+];
