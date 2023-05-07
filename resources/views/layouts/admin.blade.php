e786b64d35721e5fd2545d87c35833996a87aa91
y-hirade <yuuki.h.0909@gmail.com>
2023-05-07 04:19:11 +0000

    #21 bladeの継承レイアウト作成　細部は要編集

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,74 @@
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <!-- CSS only -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css">
+
+    </head>
+    <body>
+        <div id="app">
+            {{---->ナヴィゲーションバー--}}
+            <nav class="navbar navbar-expand-xl navbar-light bg-light">
+              <div class="container">
+                <a class="navbar-brand" href="#">平商事</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarWithDropdown" aria-controls="navbarWithDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse show" id="navbarWithDropdown">
+                  <ul class="navbar-nav">
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Dispatch Application Form</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Dispatch Request List</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Driver Calendar</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Registration Forms for Only Administrator
+                      </a>
+                      <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Department for User</a></li>
+                        <li><a class="dropdown-item" href="#">User Registration Form</a></li>
+                        <li><a class="dropdown-item" href="#">Size Category for Car</a></li>
+                        <li><a class="dropdown-item" href="#">Item for Request Item</a></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Registration Forms for User
+                      </a>
+                      <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Customer Registration Form</a></li>
+                        <li><a class="dropdown-item" href="#">Pickup Location for Customer</a></li>
+                        <li><a class="dropdown-item" href="#">Car Registration Form</a></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Management Consoles
+                      </a>
+                      <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Management for Customers</a></li>
+                        <li><a class="dropdown-item" href="#">Management for Pickup Locations</a></li>
+                        <li><a class="dropdown-item" href="#">Management for Cars</a></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+                <div>
+                    @yield('content')
+                </div>
+        </div>
+    </body>
+</html>
