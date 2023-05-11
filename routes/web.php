4f6ec15a227a2b7d93d1cb11dd6a207ee0e32c9b
y-hirade <yuuki.h.0909@gmail.com>
2023-05-11 11:56:26 +0000

    #15 顧客登録画面のview,routing実装

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,22 @@ Route::controller(CarController::class)->prefix('dispatch/')->name('dispatch.')-
     Route::get('car/add', 'add')->name('car.add');
     Route::post('car/add', 'create')->name('car.create');
     Route::get('car/index', 'index')->name('car.index');
+    Route::get('car/edit', 'edit')->name('car.edit');
+    Route::post('car/update', 'update')->name('car.update');
+    Route::get('car/delete', 'delete')->name('car.delete');
+});
+
+use App\Http\Controllers\CustomerController;
+Route::controller(CustomerController::class)->prefix('customer/')->name('customer.')->middleware('auth')->group(function() {
+    Route::get('add', 'add')->name('add');
+    Route::post('add', 'create')->name('create');
+    Route::get('index', 'index')->name('index');
+});
+
+use App\Http\Controllers\LocationController;
+Route::controller(LocationController::class)->prefix('customer/')->name('customer.')->middleware('auth')->group(function() {
+    Route::get('location/add', 'add')->name('location.add');
+    Route::get('location/add', 'create')->name('location.create');
 });
 
 Route::get('/', function () {
