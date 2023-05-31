041e4a823cf93720487253060ffc7f3d3bfd57a9
y-hirade <yuuki.h.0909@gmail.com>
2023-05-31 07:45:18 +0000

    #27 midllewareでマルチ認証を実装　途中経過

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if($request->user()->department_id != 1)
+        {
+            abort(403);
+        }
+        return $next($request);    
+        // dd($request->user()->department_id);
+        
+    }
+}
