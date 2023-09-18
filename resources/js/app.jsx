9f0fb423ed24165771ce500612ac634d0a5bc674
y-hirade <yuuki.h.0909@gmail.com>
2023-09-18 06:11:03 +0000

    2 試しに一部コンポーネント分けした。

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -16,20 +16,22 @@ const DispatchCell = ({dispatchName}) => {
   );
 }
 
-const DriverDispatch = ({driverName}) => {
+const DriverDispatchList = ({driverName}) => {
     
     return(
-        <tr>
-          <th scope="row">{driverName}</th>
-          <DispatchCell dispatchName="hoge" />
-          <DispatchCell dispatchName="hoge" />
-          <DispatchCell dispatchName="hoge" />
-          <DispatchCell dispatchName="hoge" />
-          <DispatchCell dispatchName="hoge" />
-          <DispatchCell dispatchName="hoge" />
-          <DispatchCell dispatchName="foo" />
-          <DispatchCell dispatchName="hoo" />
-        </tr>
+      <tbody>
+          {drivers.map(driver => (
+          <tr key={driver.id}>
+            <th scope="row">{driver.name}</th>
+            {
+              dispatchRequests.map(dispatch => (
+                  dispatch.driver_id == driver.id && <td><button id={dispatch.id} onClick={ handleClick } key={dispatch.id}>{dispatch.customer.name + ' (' + dispatch.location.name + ')'}</button></td>
+                )
+              )
+            }
+          </tr>
+          ))}
+      </tbody>
     );
 }
 
@@ -53,50 +55,82 @@ const ColInOrder = ({...props}) => {
 }
 
 const DriverTable = () => {
-    
+  const [drivers, setDrivers] = useState( [] );
+  const [dispatchRequests, setDispatchRequests] = useState( [] );
+  const [jsToday, setJsToday] = useState('');
+  
+  useEffect(() => {
+    const url = '/api/drivers';
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log({data});
+        setDrivers(Object.values(data['drivers']));
+        setDispatchRequests(Object.values(data['dispatch_requests']).sort(compareStartDatetime));
+        // setJsToday(data.today.toISOString());
+        setJsToday(data.today);
+        
+        // console.log(driver);
+        // console.log(data[0][0]);
+        })
+  }, []);
+  
+  const compareStartDatetime = (a,b) => {
+    if(a.start_datetime > b.start_datetime){
+      return 1;
+    }
+    if(a.start_datetime < b.start_datetime){
+      return -1;
+    }
+    return 0;
+  }
+  
     return(
     <div className="row">
-      <div className="table-responsive col">
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col" colSpan={4}>
-                午前
-              </th>
-              <th scope="col" colSpan={4}>
-                午後
-              </th>
-            </tr>
-            <ColInOrder rowName="運転手" colName="配車順" />
-          </thead>
-          <tbody>
-            <DriverDispatch driverName="川口" />
-            <DriverDispatch driverName="内海" />
-            <DriverDispatch driverName="佐々木" />
-          </tbody>
-        </table>
-      </div>
+    <div className="table-responsive, col">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">{jsToday && format(new Date(jsToday), 'yyyy/MM/dd')}</th>
+            <th scope="col" colSpan={4}>
+              午前
+            </th>
+            <th scope="col" colSpan={4}>
+              午後
+            </th>
+          </tr>
+          <tr>
+            <th scope="col">運転手 / 配車順</th>
+            <th scope="col">1</th>
+            <th scope="col">2</th>
+            <th scope="col">3</th>
+            <th scope="col">4</th>
+            <th scope="col">5</th>
+            <th scope="col">6</th>
+            <th scope="col">7</th>
+            <th scope="col">8</th>
+          </tr>
+        </thead>
+        <DriverDispatchList />
+      </table>
     </div>
+  </div>
     );
 }
 
-const DateTime = ({...dateProps}) => {
-    
-    const {date, startTime, endTime} = dateProps;
-    
+const DateTimeDetail = ({selectedDispatch}) => {
     return(
-    <div className="card">
+      <div className="card">
         <div className="card-header">日時</div>
         <div className="card-body">
           <h5 className="card-title">日付</h5>
-          <p className="card-text">{date}</p>
+          <p className="card-text">{selectedDispatch && format(new Date(selectedDispatch.start_datetime), 'yyyy/MM/dd')}</p>
         </div>
         <div className="card-body">
           <h5 className="card-title">時間帯</h5>
-          <p className="card-text">{startTime} ~ {endTime}</p>
+          <p className="card-text">{selectedDispatch && format(new Date(selectedDispatch.start_datetime), 'HH:mm') + ' ~ '}  {selectedDispatch && format(new Date(selectedDispatch.end_datetime), 'HH:mm')}</p>
         </div>
-    </div>
+      </div>
     );
 }
 
@@ -172,36 +206,95 @@ const OtherInfomation = ({...otherProps}) => {
 }
 
 const DetailDispatch = () => {
+  
+  const [selectedDispatch, setSelectedDispatch] = useState(undefined);
+  const [selectedId, setSelectedId] = useState(undefined);
+  
+  const [selectedAddress, setSelectedAddress] = useState("");
+  
+  const handleMap = (e) => {
+    console.log(e.target.id);
+    const renderingAddress = selectedDispatch.location.address;
+    setSelectedAddress(renderingAddress);
+    console.log({renderingAddress});
+    console.log({selectedAddress});
+  }
+  
     return(
-    <div>
-        <DateTime 
-            date="8月10日"
-            startTime="10:00"
-            endTime="12:00"
-        />
-        <CustomerInfomation 
-            customerName="丸一解体"
-            locationName="藤枝"
-            locationMap="#"
-        />
-        <CarInfomation 
-            sizeCategory="10"
-            ability="ヒアブ"
-            registrationNumber="浜松 た ..20"
-        />
-        <OtherInfomation 
-            item="ハコ×4"
-            method="ヒアブで積込"
-            user="村松"
-            description="現場狭いです。気を付けて。"
-        />
+  <div className="row">
+    {/* Cellをクリックしたときに表示される現場詳細 */}
+    
+    <div className="card">
+      <div className="card-header">顧客情報</div>
+      <div className="card-body">
+        <h5 className="card-title">引取先 (現場)</h5>
+        <p className="card-text">{selectedDispatch && selectedDispatch.customer.name} ({selectedDispatch && selectedDispatch.location.name})</p>
+        {/*
+        */}
+        {/* 現場詳細画面へ遷移 */}
+        <a href="#" onClick={handleMap} className="btn btn-primary" id={selectedDispatch && selectedDispatch.id}>
+          現場地図
+        </a>
+        <iframe src={`https://maps.google.co.jp/maps?output=embed&q=${selectedAddress}&z=16`} width="600" height="400" frameborder="0" scrolling="no" ></iframe>
+      </div>
     </div>
+    <div className="card">
+      <div className="card-header">車両</div>
+      <div className="card-body">
+        <h5 className="card-title">車種</h5>
+        <p className="card-text">{selectedDispatch && selectedDispatch.size_category.name + 't'}{selectedDispatch && selectedDispatch.ability.name}</p>
+        <h5 className="card-title">車番</h5>
+        <p className="card-text">{selectedDispatch && selectedDispatch.car.registration_number}</p>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">持ち物</h5>
+        <p className="card-text">{selectedDispatch && selectedDispatch.item}</p>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">引取方法</h5>
+        <p className="card-text">{selectedDispatch && selectedDispatch.method}</p>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">担当者</h5>
+        <p className="card-text">{selectedDispatch && selectedDispatch.user.name}</p>
+      </div>
+    </div>
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">詳細説明 (画像)</h5>
+        <p className="card-text">{selectedDispatch && selectedDispatch.description}</p>
+        <p className="card-text">
+          <small className="text-muted">{selectedDispatch && format(new Date(selectedDispatch.updated_at), 'yyyy/MM/dd  HH:mm') + '最終更新'}</small>
+        </p>
+      </div>
+      {selectedDispatch && selectedDispatch.image_path && (
+      selectedDispatch.image_path.split(',').map((img) => (
+      <img src={`../storage/image/${img}`}  className="card-img-bottom" alt="card-img-bottom" />
+      ))
+      
+      )}
+    </div>
+  </div>  
     );
 }
 
 function App() {
     return (
     <div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1>本日の配車</h1>
+            </div>
+          </div>
+        </div>
+    
         <DriverTable />
         <DetailDispatch />
     </div>
@@ -325,17 +418,7 @@ const Template = () => {
   </div>
   <div className="row">
     {/* Cellをクリックしたときに表示される現場詳細 */}
-    <div className="card">
-      <div className="card-header">日時</div>
-      <div className="card-body">
-        <h5 className="card-title">日付</h5>
-        <p className="card-text">{selectedDispatch && format(new Date(selectedDispatch.start_datetime), 'yyyy/MM/dd')}</p>
-      </div>
-      <div className="card-body">
-        <h5 className="card-title">時間帯</h5>
-        <p className="card-text">{selectedDispatch && format(new Date(selectedDispatch.start_datetime), 'HH:mm') + ' ~ '}  {selectedDispatch && format(new Date(selectedDispatch.end_datetime), 'HH:mm')}</p>
-      </div>
-    </div>
+    <DateTimeDetail selectedDispatch={selectedDispatch}/>
     <div className="card">
       <div className="card-header">顧客情報</div>
       <div className="card-body">
